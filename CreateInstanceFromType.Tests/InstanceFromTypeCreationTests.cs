464b3b6a1067f8497492396258f24e70a3f66937
Steve Wilkes <steve@agileobjects.co.uk>
2020-07-11 12:00:58 +0100

    Handling null arguments in runtime method

--- CreateInstanceFromType.Tests/InstanceFromTypeCreationTests.cs
+++ CreateInstanceFromType.Tests/InstanceFromTypeCreationTests.cs
@@ -1,8 +1,7 @@
-using CreateInstanceFromType.Tests.TestClasses;
-
 namespace CreateInstanceFromType.Tests
 {
     using System;
+    using TestClasses;
     using Xunit;
 
     public class InstanceFromTypeCreationTests
@@ -10,7 +9,7 @@ namespace CreateInstanceFromType.Tests
         [Fact]
         public void ShouldUseAParameterlessCtor()
         {
-            var instance = (Parameterless)CreateInstanceFromType2020DesignTimeArgs
+            var instance = (Parameterless)CreateInstanceFromType2020RuntimeArgs
                 .GetInstance(typeof(Parameterless));
 
             Assert.NotNull(instance);
@@ -19,7 +18,7 @@ namespace CreateInstanceFromType.Tests
         [Fact]
         public void ShouldUseASingleParameterCtor()
         {
-            var instance = (OneParamCtor)CreateInstanceFromType2020DesignTimeArgs
+            var instance = (OneParamCtor)CreateInstanceFromType2020RuntimeArgs
                 .GetInstance(typeof(OneParamCtor), "hello!");
 
             Assert.NotNull(instance);
@@ -29,7 +28,7 @@ namespace CreateInstanceFromType.Tests
         [Fact]
         public void ShouldUseATwoParameterCtor()
         {
-            var instance = (TwoParamCtor)CreateInstanceFromType2020DesignTimeArgs
+            var instance = (TwoParamCtor)CreateInstanceFromType2020RuntimeArgs
                 .GetInstance(typeof(TwoParamCtor), "hello again!", 123);
 
             Assert.NotNull(instance);
@@ -40,7 +39,7 @@ namespace CreateInstanceFromType.Tests
         [Fact]
         public void ShouldSelectACtorFromArguments()
         {
-            var twoParamInstance = (MultiCtor)CreateInstanceFromType2020DesignTimeArgs
+            var twoParamInstance = (MultiCtor)CreateInstanceFromType2020RuntimeArgs
                 .GetInstance(typeof(MultiCtor), "hello there!", 456);
 
             Assert.NotNull(twoParamInstance);
@@ -48,7 +47,7 @@ namespace CreateInstanceFromType.Tests
             Assert.Equal(456, twoParamInstance.IntValue);
             Assert.Equal(default, twoParamInstance.DateValue);
 
-            var oneParamInstance = (MultiCtor)CreateInstanceFromType2020DesignTimeArgs
+            var oneParamInstance = (MultiCtor)CreateInstanceFromType2020RuntimeArgs
                 .GetInstance(typeof(MultiCtor), "hello you!");
 
             Assert.NotNull(oneParamInstance);
@@ -56,7 +55,7 @@ namespace CreateInstanceFromType.Tests
             Assert.Equal(default, oneParamInstance.IntValue);
             Assert.Equal(default, twoParamInstance.DateValue);
 
-            var threeParamInstance = (MultiCtor)CreateInstanceFromType2020DesignTimeArgs
+            var threeParamInstance = (MultiCtor)CreateInstanceFromType2020RuntimeArgs
                 .GetInstance(typeof(MultiCtor), "hello blah!", 999, DateTime.MinValue);
 
             Assert.NotNull(threeParamInstance);
@@ -64,5 +63,77 @@ namespace CreateInstanceFromType.Tests
             Assert.Equal(999, threeParamInstance.IntValue);
             Assert.Equal(DateTime.MinValue, threeParamInstance.DateValue);
         }
+
+        [Fact]
+        public void ShouldHandleANullArgument()
+        {
+            var threeParamInstance = (MultiCtor)CreateInstanceFromType2020RuntimeArgs
+                .GetInstance(typeof(MultiCtor), default(string), 456, DateTime.MaxValue);
+
+            Assert.NotNull(threeParamInstance);
+            Assert.Null(threeParamInstance.StringValue);
+            Assert.Equal(456, threeParamInstance.IntValue);
+            Assert.Equal(DateTime.MaxValue, threeParamInstance.DateValue);
+        }
+
+        [Fact]
+        public void ShouldErrorIfNoMatchingConstructorExists()
+        {
+            var ctorNotFoundEx = Assert.Throws<NotSupportedException>(() =>
+            {
+                CreateInstanceFromType2020RuntimeArgs
+                    .GetInstance(typeof(OneParamCtor), DateTime.MinValue, DateTime.MaxValue);
+            });
+
+            Assert.Equal("Failed to find a matching constructor", ctorNotFoundEx.Message);
+        }
+
+        [Fact]
+        public void ShouldErrorIfNullArgumentAndNoMatchingConstructorExists()
+        {
+            var ctorNotFoundEx = Assert.Throws<NotSupportedException>(() =>
+            {
+                CreateInstanceFromType2020RuntimeArgs
+                    .GetInstance(typeof(MultiTwoParamCtor), DateTime.Now, "hello!");
+            });
+
+            Assert.Equal("Failed to find a matching constructor", ctorNotFoundEx.Message);
+        }
+
+        [Fact]
+        public void ShouldErrorIfNullArgumentAndMultipleMatchingConstructorsExist()
+        {
+            var ctorNotFoundEx = Assert.Throws<NotSupportedException>(() =>
+            {
+                CreateInstanceFromType2020RuntimeArgs
+                    .GetInstance(typeof(MultiTwoParamCtor), 123, default(string));
+            });
+
+            Assert.Contains("Failed to find a single matching constructor", ctorNotFoundEx.Message);
+            Assert.Contains("null arguments", ctorNotFoundEx.Message);
+        }
+
+        #region Test Classes
+
+        public class MultiTwoParamCtor
+        {
+            public MultiTwoParamCtor(int intValue, string stringValue)
+            {
+                IntValue = intValue;
+                StringValue = stringValue;
+            }
+
+            public MultiTwoParamCtor(int intValue, int? nullableIntValue)
+            {
+                IntValue = intValue;
+                StringValue = nullableIntValue.ToString();
+            }
+
+            public int IntValue { get; }
+            
+            public string StringValue { get; }
+        }
+
+        #endregion
     }
 }
--- CreateInstanceFromType/CreateInstanceFromType2020RuntimeArgs.cs
+++ CreateInstanceFromType/CreateInstanceFromType2020RuntimeArgs.cs
@@ -4,6 +4,7 @@
     using System.Collections.Concurrent;
     using System.Linq.Expressions;
     using System.Reflection;
+    using static System.Reflection.BindingFlags;
 
     public static class CreateInstanceFromType2020RuntimeArgs
     {
@@ -27,14 +28,10 @@
         private static Func<object[], object> CreateObjectFactory(TypeFactoryKey key)
         {
             var argumentTypes = key.ArgumentTypes;
+            var argumentCount = argumentTypes.Length;
 
             // The constructor which matches the given argument types:
-            var instanceTypeCtor = key.Type.GetConstructor(
-                BindingFlags.Instance | BindingFlags.Public,
-                binder: null,
-                CallingConventions.HasThis,
-                argumentTypes,
-                new ParameterModifier[0]);
+            var instanceTypeCtor = GetMatchingConstructor(key, argumentTypes, argumentCount);
 
             // An Expression representing the parameter to pass
             // to the Func:
@@ -42,7 +39,6 @@
 
             // A set of Expressions representing the parameters to pass
             // to the constructor:
-            var argumentCount = argumentTypes.Length;
             var ctorArguments = new Expression[argumentCount];
 
             for (var i = 0; i < argumentCount; ++i)
@@ -72,10 +68,81 @@
             return instanceCreationLambda.Compile();
         }
 
+        private static ConstructorInfo GetMatchingConstructor(
+            TypeFactoryKey key,
+            Type[] argumentTypes,
+            int argumentCount)
+        {
+            if (!key.HasNullArgumentTypes)
+            {
+                return key.Type.GetConstructor(
+                    Public | Instance,
+                    binder: null,
+                    CallingConventions.HasThis,
+                    argumentTypes,
+                    new ParameterModifier[0]) ??
+                    throw new NotSupportedException("Failed to find a matching constructor");
+            }
+
+            var constructors = key.Type.GetConstructors(Public | Instance);
+            var matchingCtor = default(ConstructorInfo);
+            var parameters = default(ParameterInfo[]);
+
+            for (int i = 0, l = constructors.Length; i < l; ++i)
+            {
+                var constructor = constructors[i];
+                parameters = constructor.GetParameters();
+
+                if (parameters.Length != argumentCount)
+                {
+                    continue;
+                }
+
+                for (var j = 0; j < argumentCount; ++j)
+                {
+                    var argumentType = argumentTypes[j];
+
+                    if ((argumentType != null) &&
+                        !parameters[j].ParameterType.IsAssignableFrom(argumentType))
+                    {
+                        goto NextConstructor;
+                    }
+                }
+
+                if (matchingCtor != null)
+                {
+                    throw new NotSupportedException(
+                        "Failed to find a single matching constructor due to null arguments");
+                }
+
+                matchingCtor = constructor;
+
+            NextConstructor:;
+            }
+
+            if (matchingCtor == null)
+            {
+                throw new NotSupportedException(
+                    "Failed to find a matching constructor due to null arguments");
+            }
+
+            for (var j = 0; j < argumentCount; ++j)
+            {
+                if (argumentTypes[j] == null)
+                {
+                    argumentTypes[j] = parameters[j].ParameterType;
+                }
+            }
+
+            return matchingCtor;
+        }
+
         #region Helper Classes
 
         private class TypeFactoryKey
         {
+            private static readonly int _nullArgumentHashCode = new Random().Next(100_000, 999_000);
+
             private readonly int _hashCode;
 
             public TypeFactoryKey(Type type, object[] arguments)
@@ -96,6 +163,15 @@
                         case 1:
                             {
                                 var argument = arguments[0];
+
+                                if (argument == null)
+                                {
+                                    ArgumentTypes = new[] { default(Type) };
+                                    HasNullArgumentTypes = true;
+                                    _hashCode = GetNullArgumentHashCodeValue();
+                                    return;
+                                }
+
                                 var argumentType = argument.GetType();
                                 ArgumentTypes = new[] { argumentType };
                                 _hashCode = GetHashCodeValue(argumentType);
@@ -108,6 +184,15 @@
                             for (var i = 0; i < argumentCount; ++i)
                             {
                                 var argument = arguments[i];
+
+                                if (argument == null)
+                                {
+                                    ArgumentTypes[i] = default;
+                                    HasNullArgumentTypes = true;
+                                    _hashCode = GetNullArgumentHashCodeValue();
+                                    continue;
+                                }
+
                                 var argumentType = argument.GetType();
                                 ArgumentTypes[i] = argumentType;
                                 _hashCode = GetHashCodeValue(argumentType);
@@ -118,12 +203,20 @@
                 }
             }
 
+            private int GetNullArgumentHashCodeValue()
+                => GetHashCodeValue(_nullArgumentHashCode);
+
             private int GetHashCodeValue(Type argumentType)
-                => (_hashCode * 397) ^ argumentType.GetHashCode();
+                => GetHashCodeValue(argumentType.GetHashCode());
+
+            private int GetHashCodeValue(int argumentTypeHashCode)
+                => (_hashCode * 397) ^ argumentTypeHashCode;
 
             public Type Type { get; }
 
             public Type[] ArgumentTypes { get; }
+
+            public bool HasNullArgumentTypes { get; }
 
             public override bool Equals(object obj)
                 => ((TypeFactoryKey)obj)._hashCode == _hashCode;
